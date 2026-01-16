37d575fba9e8b84e42b3b3156cf8c0767cfb0a56
Smail-hub <amilsafin@gmail.com>
2026-01-17 01:16:39 +0500

    Собрал картинку обратно

--- MerlinMonro.py
+++ MerlinMonro.py
@@ -1,8 +1,7 @@
 from PIL import Image
 
 image = Image.open("monro.jpg")
-print(image.mode) 
-red, green, blue = image.split() 
-red.save("red_channel.jpg")
-green.save("green_channel.jpg") 
-blue.save("blue_channel.jpg") 
+print(image.mode)
+red, blue, green = image.split()
+new_image = Image.merge("RGB", (red, blue, green))
+new_image.save("new_image.jpg")
